fa0c941b53163f6004fcec709a7beedf93d26a1f
SwierczKacper <swierczkacperkontakt@gmail.com>
2022-01-15 17:29:31 +0100

    #3 Remove welcome screen

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class DashboardController extends Controller
+{
+    public function __invoke(): Factory|View|Application
+    {
+        return view('dashboard');
+    }
+}
